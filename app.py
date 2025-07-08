8652c3edc8997c5a6ae66feba8230bf568d5b755
MisaelVa06 <mava0001@ce.pucmm.edu.do>
2025-07-07 23:14:11 -0400

    Agregue el uso de base de datos SQLALchemy, solo para guardar datos

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///contactos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Contacto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(120), nullable=False)
+    mensaje = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Contacto {self.nombre}>'
+
 
 @app.route('/')
 def inicio():
@@ -20,11 +37,15 @@ def contact():
         if not nombre or not correo or not mensaje:
              error = "Por favor completa todos los campos."
              return render_template('contact.html', error=error)
-    
-        return render_template('response.html', nombre=nombre)
-   
-    return render_template('contact.html')
+        
+        nuevo_contacto = Contacto(nombre=nombre, correo=correo, mensaje=mensaje)
+        db.session.add(nuevo_contacto)
+        db.session.commit()
 
+        return render_template('response.html', nombre=nombre)
+        
+
+    return render_template('contact.html')
 
 
 
